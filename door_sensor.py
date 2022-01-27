f543d31797cfb285777d08cbf20e8531bb0f821b
Matt Lefaive <lefaivem@gmail.com>
2022-01-27 16:27:16 -0500

    Fixed issue with my non-flush door

--- door_sensor.py
+++ door_sensor.py
@@ -12,20 +12,19 @@ NSFW_mode = False
 
 while True:
     # Light up LEDs based on button/door states
-    if not button.value():
-        red.value(1)
-    else:
-        red.value(0)
-        
-    if not door.value():
-        green.value(1)
-    else:
-        green.value(0)
+    red.value(not button.value())
+    green.value(not door.value())
     
     # If door opens, send a SFW signal
     if not door.value():
-        signal = "SFW"
-        NSFW_mode = False
+        
+        # My door is not flush with the door frame, leading to
+        # occasional "blips". Pause for 0.1 seconds to mitigate
+        # false positives
+        time.sleep(0.1)
+        if not door.value():
+            signal = "SFW"
+            NSFW_mode = False
         
     # If door closed and button gets pressed, send NSFW signal
     # Do not send a SFW signal until the door opens
